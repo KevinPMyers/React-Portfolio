37b73dff36e993de4c03b9d1008e113ce6ef5431
KevinPMyers <kmyers25@gmail.com>
2021-10-15 01:23:01 -0400

    header/hero and nav links working, need to complete contact page for email inquiry, display resume, add projects, better styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
-
+import hero from "../../assets/images/hero.jpg"
 const Nav = (props) => {
     
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-            <h2 id='navName'>
+        <nav className="navbar navbar-expand-lg"style=
+        {{ backgroundImage: `url(${hero})`}} id="hero">
+        
+            <h1 id='navName'>
                 Kevin Myers
-            </h2>
+            </h1>
             
             <ul className="nav nav-tabs">
                 <li class="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#About" onClick={() => props.handlePageChange('About')}> About </a> 
+                    <a className="nav-link h4 " aria-current="page" href="#About" onClick={() => props.handlePageChange('About')}> About </a> 
                 </li>
                 <br></br>
                 <li className="nav-item">
-                    <a className="nav-link active" href="#projects" onClick={() => props.handlePageChange('Projects')}> Projects </a>
+                    <a className="nav-link h4" href="#projects" onClick={() => props.handlePageChange('Projects')}> Projects </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link active" href="#contact" onClick={() => props.handlePageChange('Contact')}> Contact </a>
+                    <a className="nav-link h4" href="#contact" onClick={() => props.handlePageChange('Contact')}> Contact </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link active" href="#resmue" onClick={() => props.handlePageChange('Resume')}> Resume </a>
+                    <a className="nav-link h4" href="#resmue" onClick={() => props.handlePageChange('Resume')}> Resume </a>
                 </li>
                 
             </ul>
+        
         </nav>
     )
 }
