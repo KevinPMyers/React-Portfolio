c554df9f94fd45badacde0a4c69a260fdaa61add
KevinPMyers <kmyers25@gmail.com>
2021-10-21 00:49:16 -0400

    contact finished, projects and resume still need implemented

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,10 +7,17 @@ const Footer = () => {
     return (
         <Container className="footer">
             <Row>
-                <Col md={{ span: 6, offset: 3}} style={{display: 'flex', justifyContent:'space-around' }}>
-                    <img href="https://github.com/KevinPMyers" src={githubLogo} style={{ width:'50px'}} alt="github link"></img>
-                    <img href="https://www.linkedin.com/in/kevin-myers-0734041b1/" src={linkedIn} style={{ width:'50px'}} alt="linkedIn link"></img>
-                    <img href="mailto:kmyers25@gmail.com" src={email} style={{ width:'50px'}} alt="email link"></img>
+                <Col md={{ span: 6, offset: 3}} style={{display: 'flex', justifyContent:'space-around' }} >
+                    
+                        <a href="https://github.com/KevinPMyers" rel='noreferrer noopener' target="_blank">
+                        <img src={githubLogo} style={{ width:'50px'}} alt="github link" ></img>
+                        </a>
+                    <a href="https://www.linkedin.com/in/kevin-myers-0734041b1/" rel='noreferrer noopener' target="_blank">
+                    <img src={linkedIn} style={{ width:'50px'}} alt="linkedIn link" ></img>
+                    </a>
+                    <a href="mailto:kmyers25@gmail.com" rel='noreferrer noopener' target="_blank">
+                    <img src={email} style={{ width:'50px'}} alt="email link"></img>
+                    </a>
                     
                     
                 </Col>
