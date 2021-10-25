0386353a6a31af594702845d847f035fcee16488
KevinPMyers <kmyers25@gmail.com>
2021-10-25 19:35:18 -0400

    lost files from onedrive crashing, pulling from github, commiting local files that werent deleted

---
+++ src/pages/About/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const About = () => {
+
+
+    return(
+        <section className="my-5">
+            <h1 id='about'>Kevin Myers</h1>
+            <p>this is the about page</p>
+        </section>
+    
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default About;
